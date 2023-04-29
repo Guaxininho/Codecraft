0668550d24d76801b8f0e82dabf501713ab58618
guaxininho <sammydevjs@gmail.com>
2023-04-29 18:45:59 -0300

    database adicionada

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
-const titulo = document.querySelectorAll('.titulo')
-const descricao = document.querySelectorAll('.descricao')
-const codigo = document.querySelectorAll('.codigo')
+const titulo = document.querySelectorAll(".titulo");
+const descricao = document.querySelectorAll(".descricao");
+const codigo = document.querySelectorAll(".codigo");
 
-string = ["<h1>", "<h3>"]
-
-function chaves() {
-    for (let i = 0; i < descricao.length; i++) {
-
-        if (string[i].includes("<")) {
-
-            string[i] = string[i].replace("<", "&lt;")
-        }
-        if (string[i].includes(">")) {
-
-            string[i] = string[i].replace(">", "&gt;")
-        }
+async function enviandoDado() {
+  const dado = {
+    titulo: "veio do codigo",
+    descricao: "teste",
+    codigo: "teste",
+    link: "teste",
+    categoria: "teste",
+  };
+  const res = await fetch(
+    "https://njdgryzqvrufijxtomch.supabase.co/rest/v1/MainCards",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Prefer: "return=minimal",
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qZGdyeXpxdnJ1ZmlqeHRvbWNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI3OTc2NjcsImV4cCI6MTk5ODM3MzY2N30.ZS7CLdwasZtFttSXiWb415QO02Cog0X5FA625tVqwl8",
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qZGdyeXpxdnJ1ZmlqeHRvbWNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI3OTc2NjcsImV4cCI6MTk5ODM3MzY2N30.ZS7CLdwasZtFttSXiWb415QO02Cog0X5FA625tVqwl8",
+      },
+      body: JSON.stringify(dado),
     }
+  );
 }
-function substituindo() {
-    for (let i = 0; i < titulo.length; i++) {
-        codigo[i].textContent = string[i]
 
+async function buscandoDados() {
+  const res = await fetch(
+    "https://njdgryzqvrufijxtomch.supabase.co/rest/v1/MainCards",
+    {
+      method: "GET",
+      headers: {
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qZGdyeXpxdnJ1ZmlqeHRvbWNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI3OTc2NjcsImV4cCI6MTk5ODM3MzY2N30.ZS7CLdwasZtFttSXiWb415QO02Cog0X5FA625tVqwl8",
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qZGdyeXpxdnJ1ZmlqeHRvbWNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI3OTc2NjcsImV4cCI6MTk5ODM3MzY2N30.ZS7CLdwasZtFttSXiWb415QO02Cog0X5FA625tVqwl8",
+      },
     }
+  );
+  const data = await res.json();
+  console.log(data);
+  codigo[0].textContent = data[0].codigo;
 }
-chaves()
-substituindo()
 
+buscandoDados();
+enviandoDado();
 
-
-//     Para "<", use &lt; ou &#60;
-// Para ">", use &gt; ou &#62;
+// codigo[0].textContent = "oi";
